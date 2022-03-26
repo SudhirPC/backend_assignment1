a5c3b3ef683c50bff1d2ed1dc663862c4328ff2f
SudhirPC <sudhirchavhan100@gmail.com>
2022-03-26 21:46:19 +0530

    Update server.js

--- day10_fileuploadAssignment/src/server.js
+++ day10_fileuploadAssignment/src/server.js
@@ -2,11 +2,11 @@ const app = require("./index");
 
 const connect = require("./config/db");
 
-app.listen(3700, async (req, res) => {
+app.listen(3750, async (req, res) => {
   try {
     await connect();
 
-    console.log("listening to the port  3700");
+    console.log("listening to the port  3750");
   } catch (error) {
     console.log(error.message);
   }
